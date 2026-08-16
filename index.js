7fc00368efc63fef5bf1cad11c7c79d4cc54fad9
elia489LungAI <mohammed.sooo9hiy@gmail.com>
2026-08-16 22:05:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'LungCraft.aternos.me', // حط آيباد سيرفرك هنا
-  username: 'AternosBot',
-  version: '1.21.11' // حط إصدار سيرفرك هنا بالضبط (مثل 1.20.1 أو 1.21)
+  host: 'LungCraft.aternos.me',
+  port: 57104,
+  username: 'AternosBot'
 });
 
 bot.on('spawn', () => {
   console.log("Bot joined and ready!");
   
-  // حركة تلقائية كل 10 ثواني عشان ما يفصله السيرفر
+  // تكرار القفز والحركة كل 6 ثوانٍ (6000 ميللي ثانية)
   setInterval(() => {
     bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 500);
-    
-    // يلف يمين شوي عشان ماينطرد بتهمة الـ AFK
-    bot.look(bot.entity.yaw + 1.5, 0);
-  }, 10000);
+    setTimeout(() => bot.setControlState('jump', false), 500); // يفعل القفزة لنصف ثانية ثم يوقفها
+    bot.look(bot.entity.yaw + 1.5, 0); // يفر راسه شوية حتى يضمن عدم اعتباره AFK
+  }, 6000);
 });
 
 bot.on('end', () => {
   console.log("Bot disconnected, reconnecting in 5 seconds...");
-  setTimeout(() => {
-    process.exit(0);
-  }, 5000);
+  setTimeout(() => process.0_exit(0), 5000); // ملاحظة: تأكد تكون process.exit(0) مثل كودك القديم
 });
 
 bot.on('error', (err) => console.log(err));
